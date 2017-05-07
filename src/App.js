36897f4c738e572306a882f73143caf8829f3072
Yudong Wang <wangyudong53138@gmail.com>
2017-05-07 16:16:05 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <hr/>
+            <p>This is align of programming, welcome!</p>
+        </div>
+    );
+  }
+}
+
+export default App;
